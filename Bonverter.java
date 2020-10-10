65e2150eb5ba2002d9e95c3ba2d13f4caf4ddbaf
jonathanhuydieu <jonathanhuydieu@knights.ucf.edu>
2020-10-10 18:31:17 -0400

    Added default text file for demo usage

---
+++ Bonverter.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+/**
+ * Bonverter
+ */
+public class Bonverter {
+
+    public static void main(String[] args) {
+        
+        createHeader("Welcome to the B o n v e r t e r");
+        ArrayList<String> plainText = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the sentence you would like to 'Bonvert' ;)");
+        String userInput = input.nextLine();
+        for(String s : userInput.split(" ")){
+            plainText.add(s);
+        }
+        input.close();
+
+
+        //BONVERT TIME
+        plainText = bonvertText(plainText);
+
+        for(String s : plainText){
+            System.out.printf("%s ", s);
+        }
+        System.out.println();
+    
+    }
+    
+    public static void createHeader(String header)
+    {
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-==-=-=-=-=-=-=-=-=-");
+        System.out.println(header);
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-==-=-=-=-=-=-=-=-=-");
+    }
+    
+
+    public static ArrayList<String> bonvertText(ArrayList<String> words){
+        ArrayList<String> ret = new ArrayList<String>();
+
+        for(String s : words){
+            Character firstLetter = s.charAt(0);
+            //Check if the starting letter is a vowel
+            if(isVowel(firstLetter)){
+                if(Character.isUpperCase(firstLetter)){
+                    ret.add("B" + Character.toLowerCase(firstLetter) + s.substring(1));
+                }else{
+                    ret.add( (String) ("b" + firstLetter + s.substring(1)));
+                }
+            } 
+            else{
+                if(Character.isUpperCase(firstLetter)){
+                    ret.add("B" + s.substring(1));
+                }else{
+                    ret.add("b" + s.substring(1));
+                }
+            }
+
+        }
+
+        return ret;
+    }
+
+    public static boolean isVowel(Character c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return true;
+        }else if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            return true;
+        }
+        return false;
+    }
+}
